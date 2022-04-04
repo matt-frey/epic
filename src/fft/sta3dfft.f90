27e8e74bfa12e5843968a1237fc843d76210bc94
Matthias Frey <mf248@st-andrews.ac.uk>
2022-04-04 20:05:22 +0100

    add sta3dfft.f90

---
+++ src/fft/sta3dfft.f90
@@ -0,0 +1,171 @@
+module sta3dfft
+    use constants, only : zero, f12
+    use stafft
+    use sta2dfft
+    use deriv1d
+    implicit none
+
+    ! Spectral filter:
+    double precision, allocatable :: filt(:, :, :)
+
+    ! Wavenumbers::
+    double precision, allocatable :: rkx(:), hrkx(:), rky(:), hrky(:), rkz(:)
+
+    ! Quantities needed in FFTs:
+    double precision, allocatable :: xtrig(:), ytrig(:), ztrig(:)
+    integer                       :: xfactors(5), yfactors(5), zfactors(5)
+
+    contains
+
+        !::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+        subroutine init3dfft
+            double precision :: cfilt, kmax, kc
+            integer          :: nwx, nwy, kx, ky, kz
+
+            if (.not. allocated(filt)) then
+                allocate(filt(0:nz,nx,ny))
+                allocate(rkx(nx))
+                allocate(hrkx(nx))
+                allocate(rky(ny))
+                allocate(hrky(ny))
+                allocate(rkz(nz))
+                allocate(xtrig(2*nx))
+                allocate(ytrig(2*ny))
+                allocate(ztrig(2*nz))
+            endif
+
+            nwx = f12 * nx
+            nwy = f12 * ny
+
+            !----------------------------------------------------------
+            ! Set up FFTs:
+            ! Initialise FFTs and wavenumber arrays:
+            call init2dfft(nx, ny, ellx, elly, xfactors, yfactors, xtrig, ytrig, hrkx, hrky)
+            call initfft(nz, zfactors, ztrig)
+
+            !Define x wavenumbers:
+            rkx(1) = zero
+            do kx = 1, nwx-1
+                rkx(kx+1)    = hrkx(2*kx)
+                rkx(nx+1-kx) = hrkx(2*kx)
+            enddo
+            rkx(nwx+1) = hrkx(nx)
+
+            !Define y wavenumbers:
+            rky(1) = zero
+            do ky = 1, nwy-1
+                rky(ky+1)    = hrky(2*ky)
+                rky(ny+1-ky) = hrky(2*ky)
+            enddo
+            rky(nwy+1) = hrky(ny)
+
+            !Define z wavenumbers:
+            call init_deriv(nz, ellz,r kz)
+
+            !Define filter:
+
+            !kz > 0:
+            do ky = 1, ny
+                do kx = 1, nx
+                    do kz = 1, nz
+                        filt(kz, kx, ky) = rkx(kx)**2 + rky(ky)**2 + rkz(kz)**2
+                    enddo
+                enddo
+            enddo
+
+            !kz = 0:
+            do ky = 1, ny
+                do kx = 1, nx
+                    filt(0, kx, ky) = rkx(kx)**2 + rky(ky)**2
+                enddo
+            enddo
+
+            kmax = dsqrt(maxval(filt))
+!             write(*,*)
+!             write(*,'(a,f9.3)') '  Note, kx_max = ',rkx(nwx+1)
+!             write(*,'(a,f9.3)') '        ky_max = ',rky(nwy+1)
+!             write(*,'(a,f9.3)') '        kz_max = ',rkz(nz)
+!             write(*,'(a,f9.3)') '   and  k_max = ',kmax
+!             write(*,*)
+!             write(*,*) ' Enter k_c/k_max:'
+!             read(*,*) kc
+
+            kc = kc * kmax
+            cfilt = -one / kc**2
+
+            do ky = 1, ny
+                do kx = 1, nx
+                    do kz = 0, nz
+                        filt(kz, kx, ky) = dexp(cfilt * filt(kz, kx, ky))
+                    enddo
+                enddo
+            enddo
+        end subroutine
+
+        !::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+        !Computes a 3D FFT of an array fp in physical space and
+        !returns the result as fs in spectral space.  It is assumed that
+        !fp is generally non-zero at the z boundaries (so a cosine
+        !transform is used in z).
+        !*** fp is destroyed upon exit ***
+        subroutine fftczp2s(fp,fs)
+            double precision:: fp(0:nz,ny,nx)  !Physical
+            double precision:: fs(0:nz,nx,ny)  !Spectral
+            integer:: kx,ky,iy
+
+            !Carry out a full x transform first:
+            call forfft((nz+1)*ny, nx, fp, xtrig, xfactors)
+
+            !Transpose array:
+            do kx = 1, nx
+                do iy = 1, ny
+                    fs(:, kx, iy) = fp(:, iy, kx)
+                enddo
+            enddo
+
+            !Carry out a full y transform on transposed array:
+            call forfft((nz+1)*nx, ny, fs, ytrig, yfactors)
+
+            !Carry out z FFT for each kx and ky:
+            do ky = 1, ny
+                do kx = 1, nx
+                    call dct(1, nz, fs(:, kx, ky), ztrig, zfactors)
+                enddo
+            enddo
+        end subroutine
+
+        !::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+        !Computes an *inverse* 3D FFT of an array fs in spectral space and
+        !returns the result as fp in physical space.  It is assumed that
+        !fp is generally non-zero at the z boundaries (so a cosine
+        !transform is used in z).
+        !*** fs is destroyed upon exit ***
+        subroutine fftczs2p(fs,fp)
+            double precision, intent(inout) :: fs(0:nz, nx, ny)  !Spectral
+            double precision, intent(out)   :: fp(0:nz, ny, nx)  !Physical
+            integer                         :: ix, iy, kx
+
+            !Carry out a full inverse y transform first:
+            call revfft((nz+1)*nx,ny,fs,ytrig,yfactors)
+
+            !Transpose array:
+            do kx = 1, nx
+                do iy = 1, ny
+                    fp(:, iy, kx) = fs(:, kx, iy)
+                enddo
+            enddo
+
+            !Carry out a full inverse x transform:
+            call revfft((nz+1)*ny, nx, fp, xtrig, xfactors)
+
+            !Carry out z FFT for each ix and iy:
+            do ix = 1, nx
+                do iy = 1, ny
+                    call dct(1, nz, fp(:, iy, ix), ztrig, zfactors)
+                enddo
+            enddo
+        end subroutine
+
+end module sta3dfft
